2ed1e61ac629c3401b46d64e34cadc341e4cdc23
simone.capone <simo.capone.1997.7@outlook.it>
2024-12-22 16:52:21 +0100

    #1 - Inizializzazione servizio

---
+++ src/main/java/myproject/MainApp.java
@@ -0,0 +1,9 @@
+package myproject;
+
+import io.quarkus.runtime.Quarkus;
+
+public class MainApp {
+    public static void main(String... args) {
+        Quarkus.run(args); // Avvia l'applicazione Quarkus
+    }
+}
